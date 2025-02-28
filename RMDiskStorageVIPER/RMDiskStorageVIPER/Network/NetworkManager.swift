3bfa57661de22edcf889cbfab2a2a6403a9319aa
ronesta <gvozzzd123@gmail.com>
2025-02-28 17:41:34 +0300

    Edits

--- RMDiskStorageVIPER/RMDiskStorageVIPER/Network/NetworkManager.swift
+++ RMDiskStorageVIPER/RMDiskStorageVIPER/Network/NetworkManager.swift
@@ -6,16 +6,16 @@
 //
 
 import Foundation
-import UIKit
+import UIKit.UIImage
 
 final class NetworkManager: NetworkManagerProtocol {
-    var dataCounter = 1
-    var imageCounter = 1
+    private var dataCounter = 1
+    private var imageCounter = 1
 
-    var storageManager: StorageManagerProtocol?
+    private let storageManager: StorageManagerProtocol
     private let urlString = "https://rickandmortyapi.com/api/character"
 
-    init(storageManager: StorageManagerProtocol?) {
+    init(storageManager: StorageManagerProtocol) {
         self.storageManager = storageManager
     }
 
@@ -62,7 +62,7 @@ final class NetworkManager: NetworkManagerProtocol {
     func loadImage(from urlString: String, completion: @escaping (UIImage?) -> Void) {
         let key = urlString.addingPercentEncoding(withAllowedCharacters: .urlHostAllowed) ?? urlString
 
-        if let imageData = storageManager?.loadImage(key: key),
+        if let imageData = storageManager.loadImage(key: key),
            let image = UIImage(data: imageData) {
             completion(image)
             return
@@ -84,7 +84,7 @@ final class NetworkManager: NetworkManagerProtocol {
 
             if let data,
                let image = UIImage(data: data) {
-                self.storageManager?.saveImage(data, key: key)
+                self.storageManager.saveImage(data, key: key)
                 DispatchQueue.main.async {
                     completion(image)
                     print("Load image \(self.imageCounter)")
