c0c1659f2dc791bbb7a6a36f2ac5ecd0b53cff80
ronesta <gvozzzd123@gmail.com>
2025-04-06 22:04:09 +0300

    CheckPoint

---
+++ RMDiskStorageVIPER/CharactersTests/Characters/View/MockCharactersView.swift
@@ -0,0 +1,21 @@
+//
+//  MockCharactersView.swift
+//  RMDiskStorageVIPER
+//
+//  Created by Ибрагим Габибли on 06.04.2025.
+//
+
+@testable import RMDiskStorageVIPER
+
+final class MockCharactersView: CharactersViewProtocol {
+    private(set) var characters: [Character]?
+    private(set) var errorMessage: String?
+
+    func displayCharacters(_ characters: [Character]) {
+        self.characters = characters
+    }
+
+    func displayError(_ message: String) {
+        errorMessage = message
+    }
+}
